db6d97c4b7c51020489a0e724084e58bfa53d590
leandroauzier <leandroauzier02@gmail.com)>
2021-05-19 15:27:10 -0300

    Adicionado Model Parametros

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Parametros(models.Model):
+    username = models.CharField(max_length=84, null=True, blank=True)
+    password = models.CharField(max_length=128, null=True, blank=True)
+    empresa = models.CharField(max_length=84, null=True, blank=True)
+    empresa_id = models.CharField(max_length=84, null=True, blank=True)
+    user_db = models.CharField(max_length=84, null=True, blank=True)
+    password_db = models.CharField(max_length=84, null=True, blank=True)
+    host_db = models.CharField(max_length=84, null=True, blank=True)
+    port_db = models.CharField(max_length=84, null=True, blank=True)
+    service_db = models.CharField(max_length=84, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
