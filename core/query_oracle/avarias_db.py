39210aa8918771434c8f9decc9cba26f16edef83
leandroauzier <leandroauzier02@gmail.com)>
2021-05-24 08:45:33 -0300

    correção avarias_db

--- core/query_oracle/avarias_db.py
+++ core/query_oracle/avarias_db.py
@@ -1,2 +1,7 @@
-def avarias_oracle():
-    pass
+import cx_Oracle
+import pandas as pd
+
+
+def avarias_db():
+    df_avaria = pd.read_csv('https://raw.githubusercontent.com/cluster-desenvolvimento/datasets/main/Avaria.csv', sep=';')
+    return df_avaria
